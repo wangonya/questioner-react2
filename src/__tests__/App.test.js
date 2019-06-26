3a7328e8154711f0c6defff44aa3df70a5902d4a
wangonya <kwangonya@gmail.com>
2019-06-26 16:24:21 +0300

    test with enzyme

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
+
 import App from "./../components/App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("<App /> root component", () => {
+  test("renders without crashing", () => {
+    expect(shallow(<App />)).toMatchSnapshot();
+  });
+
+  test("renders <Navbar /> component", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("TopNav")).toHaveLength(1);
+  });
 });
