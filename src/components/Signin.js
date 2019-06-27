63a050136775588e62eef350790e0620148c170d
wangonya <kwangonya@gmail.com>
2019-06-27 15:42:28 +0300

    add signin logic

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,11 +1,78 @@
 import React, { Component } from "react";
+import { Spinner, Row, Col, Button, FormGroup, Label } from "reactstrap";
+import { Form, Control } from "react-redux-form";
+import { connect } from "react-redux";
+import { Redirect } from "react-router";
 
-export default class Signin extends Component {
-  render() {
+import { signIn } from "../actions/signinActions";
+import { loadingAction } from "../actions/loadingActions";
+
+export class Signin extends Component {
+  handleSubmit(data) {
+    this.props.loadingAction();
+    this.props.signIn(data);
+  }
+
+  renderLoading() {
+    if (this.props.isSignedIn) {
+      return <Redirect to="/" />;
+    }
     return (
-      <div>
-        <p>Signin page</p>
+      <div className="pt-5 m-5 d-flex justify-content-center">
+        <Spinner type="grow" color="danger" data-test="spinner-test" />
       </div>
     );
   }
+
+  renderForm() {
+    return (
+      <Row>
+        <Col sm="12" md={{ size: 4, offset: 4 }}>
+          <Form
+            model="signin"
+            className="pt-5"
+            onSubmit={data => this.handleSubmit(data)}
+          >
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Control.text
+                className="form-control"
+                model="signin.email"
+                type="email"
+                placeholder="email"
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Control.text
+                type="password"
+                placeholder="password"
+                model="signin.password"
+                className="form-control"
+                required
+              />
+            </FormGroup>
+            <Button>Sign in</Button>
+          </Form>
+        </Col>
+      </Row>
+    );
+  }
+
+  render() {
+    return (
+      <div>{this.props.loading ? this.renderLoading() : this.renderForm()}</div>
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  isSignedIn: state.signin.isSignedIn,
+  loading: state.loading.loading
+});
+
+export default connect(
+  mapStateToProps,
+  { signIn, loadingAction }
+)(Signin);
