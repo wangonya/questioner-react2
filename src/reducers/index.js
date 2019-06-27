93b63f753102a93e672eed4b95819fc64ae5c7cd
wangonya <kwangonya@gmail.com>
2019-06-27 15:45:05 +0300

    add signin, loading reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,14 @@
 import { combineReducers } from "redux";
+import { createForms } from "react-redux-form";
 
 import meetupReducer from "./meetupReducer";
+import signinReducer from "./signinReducer";
+import loadingReducer from "./loadingReducer";
 
 export default combineReducers({
-  meetups: meetupReducer
+  meetups: meetupReducer,
+  loading: loadingReducer,
+  ...createForms({
+    signin: signinReducer
+  })
 });
